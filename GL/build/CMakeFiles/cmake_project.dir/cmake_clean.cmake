3eb73552a280712be1294e09847f5034979d843f
jikim0907 <zelretch4@gmail.com>
2024-01-09 17:35:07 -0800

    base dir

---
+++ GL/build/CMakeFiles/cmake_project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake_project.dir/src/main.cpp.o"
+  "cmake_project"
+  "cmake_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmake_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
