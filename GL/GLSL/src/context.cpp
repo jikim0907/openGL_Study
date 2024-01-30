a581cb8911465218f35382bcbc046b4498b0037d
= <zelretch4@gmail.com>
2024-01-30 05:41:24 -0800

    texture.vs .fs file has been added

--- GL/GLSL/src/context.cpp
+++ GL/GLSL/src/context.cpp
@@ -12,44 +12,31 @@ ContextUPtr Context::Create()
 
 bool Context::Init()
 {
-    // float vertices[] = {
-    //     0.5f, 0.5f, 0.0f,   // top right
-    //     0.5f, -0.5f, 0.0f,  // bottom right
-    //     -0.5f, -0.5f, 0.0f, // bottom left
-    //     -0.5f, 0.5f, 0.0f,  // top left
-    // };
-
-    float vertices[] = {
-        0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,   // top right, red
-        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,  // bottom right, green
-        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // bottom left, blue
-        -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f,  // top left, yellow
+    float vertices[] = { // x, y, z, r, g, b, s, t
+        0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+        -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f,
     };
-
     uint32_t indices[] = {
-        // note that we start from 0!
         0, 1, 3, // first triangle
         1, 2, 3, // second triangle
     };
 
     m_vertexLayout = VertexLayout::Create();
-    // m_vertexBuffer = Buffer::CreateWithData(GL_ARRAY_BUFFER, GL_STATIC_DRAW,vertices, sizeof(float) * 12);
-    m_vertexBuffer = Buffer::CreateWithData(GL_ARRAY_BUFFER, GL_STATIC_DRAW, vertices, sizeof(float) * 24);
-    // m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
-    m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0); //number 0 attribute
-    m_vertexLayout->SetAttrib(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, sizeof(float) * 3); // number 1 attribute, sizeof(float) * 3 is offset
-    //auto stride
-    m_indexBuffer = Buffer::CreateWithData(GL_ELEMENT_ARRAY_BUFFER, GL_STATIC_DRAW,indices, sizeof(uint32_t) * 6);
-    
+    m_vertexBuffer = Buffer::CreateWithData(GL_ARRAY_BUFFER, GL_STATIC_DRAW, vertices, sizeof(float) * 32); //8 col * 4 raw =32 float size
+    m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, 0);                 //sizeof(float) * 8 is stride, number 0 attribute
+    m_vertexLayout->SetAttrib(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, sizeof(float) * 3); // number 1 attribute, sizeof(float) * 3 is offset
+    m_vertexLayout->SetAttrib(2, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 8, sizeof(float) * 6);
+    // auto stride
+    m_indexBuffer = Buffer::CreateWithData(GL_ELEMENT_ARRAY_BUFFER, GL_STATIC_DRAW, indices, sizeof(uint32_t) * 6);
+
     glBindVertexArray(0); // unbind
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-    // ShaderPtr vertShader = Shader::CreateFromFile("./shader/simple.vs", GL_VERTEX_SHADER);
-    // ShaderPtr fragShader = Shader::CreateFromFile("./shader/simple.fs", GL_FRAGMENT_SHADER);
+    ShaderPtr vertShader = Shader::CreateFromFile("./shader/texture.vs", GL_VERTEX_SHADER);
+    ShaderPtr fragShader = Shader::CreateFromFile("./shader/texture.fs", GL_FRAGMENT_SHADER);
 
-    ShaderPtr vertShader = Shader::CreateFromFile("./shader/per_vertex_color.vs", GL_VERTEX_SHADER);
-    ShaderPtr fragShader = Shader::CreateFromFile("./shader/per_vertex_color.fs", GL_FRAGMENT_SHADER);
-    
     if (!vertShader || !fragShader)
     {
         return false;
@@ -68,9 +55,10 @@ bool Context::Init()
     SPDLOG_INFO("program id: {}", m_program->Get());
 
     auto image = Image::Load("./data/container.jpg");
-    if (!image){
+    if (!image)
+    {
         return false;
-        }
+    }
     SPDLOG_INFO("image: {}x{}, {} channels", image->GetWidth(), image->GetHeight(), image->GetChannelCount());
 
     glGenTextures(1, &m_texture);
@@ -80,15 +68,7 @@ bool Context::Init()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB,
-                 image->GetWidth(), image->GetHeight(), 0,
-                 GL_RGB, GL_UNSIGNED_BYTE, image->GetData());
-
-    // //input uniform
-    // auto loc = glGetUniformLocation(m_program->Get(), "color"); //"color" means uniform vec4 color in simple.fs;
-    // // glGetUniformLocation function : get name of "color" variable from fs file that is linked in program instance. 
-    // m_program->Use();
-    // glUniform4f(loc, 1.0f, 1.0f, 0.0f, 1.0f);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB,image->GetWidth(), image->GetHeight(), 0,GL_RGB, GL_UNSIGNED_BYTE, image->GetData());
 
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
@@ -100,22 +80,8 @@ void Context::Render()
     glClear(GL_COLOR_BUFFER_BIT);
 
     m_program->Use();
-    glBindVertexArray(m_vertexLayout->Get()); //have to add it !!!
-    
+    glBindVertexArray(m_vertexLayout->Get()); // have to add it !!!
+
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     glFlush();
-
-    // glClear(GL_COLOR_BUFFER_BIT);
-
-    // static float time = 0.0f;
-    // float t = sinf(time) * 0.5f + 0.5f; //is like t = sinx ...
-
-    // auto loc = glGetUniformLocation(m_program->Get(), "color");
-    // m_program->Use();
-    // glBindVertexArray(m_vertexLayout->Get()); // have to add it !!!
-    // glUniform4f(loc, t * t, 2.0f * t * (1.0f - t), (1.0f - t) * (1.0f - t), 1.0f);
-    
-    // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
-    // time += 0.016f;
 }
